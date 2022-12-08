8db32165c3f52281ea04b0ad1b73683c7276038f
AndreBlankholm <ablankholm@gmail.com>
2022-12-08 16:30:48 -0600

    added new filesystem ability

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const fs = require("fs"); // requiring the filesystem's module  //
+
 const generatePage = require('./src/page-template');
 
 
 const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+const [name, github, fart] = profileDataArgs;
+
+
 
 fs.writeFile("index.html", generatePage(name, github), (err) => {
   if (err) throw new Error(err);
-
+  
   console.log("Portfolio complete! Check out index.html to see the output!");
 });
+
+
