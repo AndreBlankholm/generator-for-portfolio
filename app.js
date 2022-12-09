de06a04663b5b29a3dee9b8e1b494dc22054cbb2
AndreBlankholm <ablankholm@gmail.com>
2022-12-08 19:35:33 -0600

    added npm

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const fs = require("fs"); // requiring the filesystem's module  //
+const inquirer = require('inquirer');
+console.log(inquirer);
+//const fs = require("fs"); // requiring the filesystem's module  //
 
-const generatePage = require('./src/page-template');
-
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github, fart] = profileDataArgs;
+//const generatePage = require('./src/page-template');
 
 
+//onst pageHTML = generatePage(name, github);
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-  
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+
+
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
 
 
